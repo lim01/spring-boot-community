a41bb06d440a7a81da2eebb451f6d7f3b7c7ce0a
lim01 <lim01@naver.com>
2021-09-05 18:23:14 +0900

    # 2021-09-05 18:23

---
+++ src/main/java/kr/co/livetour/demo/controller/usrArticleController.java
@@ -0,0 +1,39 @@
+package kr.co.livetour.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.livetour.demo.vo.Article;
+
+@Controller
+public class usrArticleController {
+	private int lastArticleId;
+	private List<Article> articles;
+	
+	public usrArticleController() {
+		lastArticleId = 0;
+		articles = new ArrayList<>();
+	}
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		int id = lastArticleId + 1;
+		Article article = new Article(id, title, body);
+		
+		articles.add(article);
+		lastArticleId = id;
+		
+		return article;
+	}
+	
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return articles;
+	}
+}
